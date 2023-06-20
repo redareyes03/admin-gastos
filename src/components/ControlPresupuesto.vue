efffb35e513c4799176e89a0f70d803601bfc2cb
Ruben Reyes <rubenreyesdaniel@gmail.com>
2023-06-20 12:36:39 -0600

    Componente para controlar gastos

---
+++ src/components/ControlPresupuesto.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="col-2">
+        <div class="contenedor-grafico"></div>
+    </div>
+    <div class="contenedor-presupuesto">
+        <button class="reset-app">Resetear app</button>
+
+        <p>
+            <span>Presupuesto: $0</span>
+        </p>
+        <p>
+            <span>Disponible: $0</span>
+        </p>
+        <p>
+            <span>Gastado: $0</span>
+        </p>
+    </div>
+</template>
